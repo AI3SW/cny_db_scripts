63304f7f266dd033c9ce025f317b5068da15992b
Kian Boon Koh <kianboonkoh@gmail.com>
2021-01-04 10:02:22 +0800

    update create tables script

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,12 +1,23 @@
 -- drop tables 
-DROP TABLE IF EXISTS public."asr_prediction";
+DROP TABLE IF EXISTS public."asr_audio_stream_prediction";
+
+DROP TABLE IF EXISTS public."asr_audio_stream_info";
+
 DROP TABLE IF EXISTS public."session";
+
 DROP TABLE IF EXISTS public."device";
+
 DROP TABLE IF EXISTS public."app";
 
+DROP TABLE IF EXISTS public."user";
+
+-- public."user" definition
+CREATE TABLE public."user" (
+	user_id SERIAL NOT NULL,
+	PRIMARY KEY (user_id)
+);
 
 -- public.app definition
-
 CREATE TABLE public."app" (
 	app_id SERIAL NOT NULL,
 	app_name VARCHAR NOT NULL,
@@ -15,7 +26,6 @@ CREATE TABLE public."app" (
 );
 
 -- public.device definition
-
 CREATE TABLE public."device" (
 	device_id VARCHAR(64) NOT NULL,
 	start_time TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
@@ -24,41 +34,68 @@ CREATE TABLE public."device" (
 );
 
 CREATE INDEX ix_last_play_time ON public.device USING btree (last_play_time);
+
 CREATE INDEX ix_device_start_time ON public.device USING btree (start_time);
 
-
 -- public."session" definition
-
 CREATE TABLE public."session" (
 	session_id VARCHAR(100) NOT NULL,
 	device_id VARCHAR(64),
 	session_start TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
 	session_end TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
 	PRIMARY KEY (session_id),
-	FOREIGN KEY(device_id) REFERENCES device (device_id) ON UPDATE CASCADE ON DELETE CASCADE
+	FOREIGN KEY(device_id) REFERENCES device (device_id) ON UPDATE CASCADE
 );
 
 CREATE INDEX ix_session_device_id ON public.session USING btree (device_id);
+
 CREATE INDEX ix_session_session_end ON public.session USING btree (session_end);
+
 CREATE INDEX ix_session_session_start ON public.session USING btree (session_start);
 
-
--- public.asr_prediction definition
-
-CREATE TABLE public."asr_prediction" (
-	asr_prediction_id SERIAL NOT NULL,
-	session_id VARCHAR, 
-	app_id INTEGER NOT NULL, 
-	input_audio BYTEA NOT NULL, 
-	receive_time TIMESTAMP WITH TIME ZONE NOT NULL,
-	end_time TIMESTAMP WITH TIME ZONE NOT NULL,
-	prediction VARCHAR, 
-	PRIMARY KEY (asr_prediction_id), 
-	FOREIGN KEY(session_id) REFERENCES session (session_id) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY(app_id) REFERENCES app (app_id) ON UPDATE CASCADE ON DELETE CASCADE
+-- public.asr_audio_stream_info definition
+CREATE TABLE public."asr_audio_stream_info" (
+	user_id INTEGER NOT NULL,
+	device_id VARCHAR(64),
+	session_id VARCHAR,
+	seq_id INTEGER,
+	app_id INTEGER NOT NULL,
+	proc_start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+	proc_end_time TIMESTAMP WITH TIME ZONE NOT NULL,
+	stream_duration BIGINT NOT NULL,
+	PRIMARY KEY (session_id, seq_id),
+	FOREIGN KEY(user_id) REFERENCES "user" (user_id) ON UPDATE CASCADE,
+	FOREIGN KEY(device_id) REFERENCES device (device_id) ON UPDATE CASCADE,
+	FOREIGN KEY(session_id) REFERENCES session (session_id) ON UPDATE CASCADE,
+	FOREIGN KEY(app_id) REFERENCES app (app_id) ON UPDATE CASCADE
 );
 
-CREATE INDEX ix_asr_prediction_app_id ON public.asr_prediction USING btree (app_id);
-CREATE INDEX ix_asr_prediction_receive_time ON public.asr_prediction USING btree (receive_time);
-CREATE INDEX ix_asr_prediction_end_time ON public.asr_prediction USING btree (end_time);
-CREATE INDEX ix_asr_prediction_session_id ON public.asr_prediction USING btree (session_id);
+CREATE INDEX ix_asr_audio_stream_info_user_id ON public.asr_audio_stream_info USING btree (user_id);
+
+CREATE INDEX ix_asr_audio_stream_info_app_id ON public.asr_audio_stream_info USING btree (app_id);
+
+CREATE INDEX ix_asr_audio_stream_info_proc_start_time ON public.asr_audio_stream_info USING btree (proc_start_time);
+
+-- public.asr_audio_stream_prediction definition
+CREATE TABLE public."asr_audio_stream_prediction" (
+	user_id INTEGER NOT NULL,
+	device_id VARCHAR(64),
+	session_id VARCHAR,
+	seq_id INTEGER,
+	app_id INTEGER NOT NULL,
+	pred_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+	input_audio BYTEA NOT NULL,
+	input_word VARCHAR NOT NULL,
+	return_text VARCHAR NOT NULL,
+	PRIMARY KEY (session_id, seq_id),
+	FOREIGN KEY(user_id) REFERENCES "user" (user_id) ON UPDATE CASCADE,
+	FOREIGN KEY(device_id) REFERENCES device (device_id) ON UPDATE CASCADE,
+	FOREIGN KEY(session_id) REFERENCES session (session_id) ON UPDATE CASCADE,
+	FOREIGN KEY(app_id) REFERENCES app (app_id) ON UPDATE CASCADE
+);
+
+CREATE INDEX ix_asr_audio_stream_prediction_user_id ON public.asr_audio_stream_prediction USING btree (user_id);
+
+CREATE INDEX ix_asr_audio_stream_prediction_app_id ON public.asr_audio_stream_prediction USING btree (app_id);
+
+CREATE INDEX ix_asr_audio_stream_prediction_pred_timestamp ON public.asr_audio_stream_prediction USING btree (pred_timestamp);
